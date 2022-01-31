f1c3c3fa9791989487b8deecb0c5df70692b9a80
J. Harshbarger <hypercubed@gmail.com>
2022-01-30 17:57:20 -0700

    works with itertools

---
+++ src/lib/itertools.spec.ts
@@ -0,0 +1,41 @@
+import { Randomator } from './randomator';
+import { enumerate, itake, izip } from 'itertools';
+
+describe('works with itertools', () => {
+  const ab = Randomator.from(() => (Math.random() < 0.5 ? 'a' : 'b'));
+  const cd = Randomator.from(() => (Math.random() < 0.66 ? 'c' : 'd'));
+
+  test('take', () => {
+    const a = Array.from(itake(5, ab));
+
+    expect(a).toHaveLength(5);
+
+    a.forEach(v => {
+      expect(v).toMatch(/[ab]/);
+    });
+  });
+
+  test('izip', () => {
+    const z = izip(ab, cd);
+    const a = Array.from(itake(5, z));
+
+    expect(a).toHaveLength(5);
+
+    a.forEach(v => {
+      expect(v[0]).toMatch(/[ab]/);
+      expect(v[1]).toMatch(/[cd]/);
+    });
+  });
+
+  test('enumerate', () => {
+    const f = itake(5, ab);
+    const a = Array.from(enumerate(f));
+
+    expect(a).toHaveLength(5);
+
+    a.forEach(v => {
+      expect(typeof v[0]).toBe('number');
+      expect(v[1]).toMatch(/[ab]/);
+    });
+  });
+});
